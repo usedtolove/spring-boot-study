099aba63d3036cbabab4fb90c1309c0af33f39ee
unknown <usedtolove@gmail.com>
2014-12-19 11:45:49 +0800

    add web code

--- 02/src/main/java/com/mycompany/study/web/CustomerController.java
+++ 02/src/main/java/com/mycompany/study/web/CustomerController.java
@@ -4,9 +4,11 @@ import com.mycompany.study.dao.CustomerDao;
 import com.mycompany.study.entity.Customer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * User: Hu Jing Ling
@@ -17,6 +19,11 @@ public class CustomerController {
 
     @Resource
     private CustomerDao customerDao;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello(){
+        return "Hello World!";
+    }
 
     @RequestMapping(value = "/customer/save")
     public String save(){
@@ -31,5 +38,11 @@ public class CustomerController {
     public Customer get(@PathVariable int id){
         Customer customer = customerDao.findOne(id);
         return customer;
+    }
+
+    @RequestMapping(value = "/customers", method= RequestMethod.GET)
+    public List<Customer> list(){
+        List<Customer> customerList = (List<Customer>) customerDao.findAll();
+        return customerList;
     }
 }
